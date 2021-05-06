5be9938eb054ccc13a3023a148bb7bacf50be9a2
Jiheon Lee <wlgjsdl0@gmail.com>
2021-05-06 18:05:38 +0900

    Add proxy file volume limit 50mb

--- config/proxy.js
+++ config/proxy.js
@@ -2,8 +2,9 @@ const proxy = require('express-http-proxy');
 
 module.exports = (app) => {
   app.use('/proxy', proxy('http://localhost:3000', {
+    limit: '50mb',
     proxyReqPathResolver: async (req) => {
-      // return 'http://localhost:3000/upload?id=' + req.params.id;
+      // return 'http://localhost:3000/upload?id=' + req.query.id;
       return 'http://localhost:3000/upload';
     },
     // proxyReqOptDecorator: (proxyReqOpts) => {
